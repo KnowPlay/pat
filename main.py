21eef37f02caaa86aa71fbedc2311303b2c9ea33
Sarah Lin <xiaowei.lin@outlook.com>
2024-04-13 09:32:30 -0400

    setup

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# route
+
+@app.route("/")
+def home():
+    return "Home"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
